2f87b7921efd32267ebff31e69757f9c64e1170f
dumdumrobots <joaquin.cornejo@utec.edu.pe>
2023-07-01 14:06:18 -0700

    Adding a current control based code.

--- motor_control/src/motor_control.py
+++ motor_control/src/motor_control.py
@@ -32,38 +32,50 @@ else:
 class Robot(object):
 
     # --- Joint Variables
-    pwm_desired = np.array([400,200]) # --- Desired Position 
-    DXL_pwm_present = np.array([0,0]) # --- Current Position
+    position_desired = np.array([2048,2148,2048,2048,2048]) # --- Desired Position 
+    pwm_desired = np.array([200,200,200,200,200]) 
+    DXL_pwm_present = np.array([0,0,0,0,0]) 
+    DXL_position_present = np.array([2048,2048,2048,2048,2048]) # --- Present Position
 
     # --- General Variables
-    DXL_ID = np.array([0,1])
+    DXL_ID = np.array([0,1,2,3,4])
 
     # --- Addresses EEPROM
     ADDR_OP_MODE = 11
     ADDR_PWM_LIMIT = 36
+    ADDR_MAX_POS_LIMIT = 48
+    ADDR_MIN_POS_LIMIT = 52
+    #125 Current 
 
     # --- Addresses RAM
     ADDR_TORQUE_EN = 64
     ADDR_LED_EN = 65 
+    ADDR_GOAL_POS = 116
+    ADDR_PRESENT_POS = 132
     ADDR_GOAL_PWM = 100
     ADDR_PRESENT_PWM = 124
 
     # --- Byte Length
     LEN_GOAL_PWM = 2
     LEN_PRESENT_PWM = 2
+    LEN_GOAL_POS = 4
+    LEN_PRESENT_POS = 4
 
 
     # --- General Settings
     PROT_VR = 2.0
-    BRATE = 57600
+    BRATE = 1000000
     DEVICE = '/dev/ttyUSB0'
 
     portHandler = PortHandler(DEVICE)
     packetHandler = PacketHandler(PROT_VR)
 
     # --- Sync write and read instances
-    pwm_groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_PWM, LEN_GOAL_PWM)
-    pwm_groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_PWM, LEN_PRESENT_PWM)
+    #pwm_groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_PWM, LEN_GOAL_PWM)
+    #pwm_groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_PWM, LEN_PRESENT_PWM)
+
+    pos_groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_POS, LEN_GOAL_POS)
+    pos_groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_POS, LEN_PRESENT_POS)
 
 
 
@@ -89,11 +101,11 @@ class Robot(object):
 
 
         # --- ROS COMMS
-        self.pwmSubscriber = rospy.Subscriber("pwm_goal_value",Int32MultiArray, self.update_goal_pwm)
-        rospy.sleep(0.005)
+        self.posSubscriber = rospy.Subscriber("pos_goal_value",Int32MultiArray, self.update_goal_position)
+        rospy.sleep(0.005) #pwm_goal_value
 
-        self.pwmPublisher = rospy.Publisher("pwm_present_value",Int32MultiArray, queue_size=10)
-        rospy.sleep(0.005)
+        self.posPublisher = rospy.Publisher("pos_present_value",Int32MultiArray, queue_size=10)
+        rospy.sleep(0.005) #pos_goal_value
 
 
         # --- Set up Servomotors 
@@ -102,8 +114,13 @@ class Robot(object):
             dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_LED_EN, True)
             
             # --- Define PWM Control / -885(-100%) to 885(100%) at 0.113% per unit 
-            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_OP_MODE, 16)
+            # --- Define Position Control / 4095 to 0 at 0.088 / 512 (45deg)
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_OP_MODE, 3)
+            
             dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, ID, self.ADDR_PWM_LIMIT, 885) 
+
+            dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, ID, self.ADDR_MAX_POS_LIMIT, 2560)
+            dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, ID, self.ADDR_MIN_POS_LIMIT, 1536)
 
             dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_TORQUE_EN, True)
 
@@ -124,10 +141,6 @@ class Robot(object):
                 print("Motor ID: " + str(ID) + " ready to use!\n")
 
 
-            # --- Storage parameters for Present PWM
-            
-                        
-
 
     def update_goal_pwm(self,msg):
 
@@ -140,6 +153,17 @@ class Robot(object):
             print("Invalid PWM array, try again.\n")
 
 
+    def update_goal_position(self,msg):
+
+        data_array = msg.data # --- Int32MultiArray data
+
+        if len(data_array) == len(self.DXL_ID):
+            self.position_desired = np.array(data_array)
+
+        else:
+            print("Invalid Position array, try again.\n")
+
+
 
     def publish_present_pwm(self):
 
@@ -149,14 +173,22 @@ class Robot(object):
         self.pwmPublisher.publish(pub_array)
 
 
+    def publish_present_position(self):
+
+        pub_array = Int32MultiArray()
+        pub_array.data = self.DXL_position_present.tolist()
+
+        print(pub_array)
+
+        self.posPublisher.publish(pub_array)
+    
+
 
     def set_goal_pwm(self):
 
         for ID in self.DXL_ID:
 
             param_goal_pwm = [DXL_LOBYTE(self.pwm_desired[ID]), DXL_HIBYTE(self.pwm_desired[ID])]
-            
-            print(param_goal_pwm)
 
             dxl_addparam_result = self.pwm_groupSyncWrite.addParam(ID, param_goal_pwm)
 
@@ -177,6 +209,34 @@ class Robot(object):
             self.shutdown()
 
         self.pwm_groupSyncWrite.clearParam()
+
+
+    def set_goal_position(self):
+
+        for ID in self.DXL_ID:
+
+            param_goal_position = [DXL_LOBYTE(DXL_LOWORD(self.position_desired[ID])), DXL_HIBYTE(DXL_LOWORD(self.position_desired[ID])),
+                                   DXL_LOBYTE(DXL_HIWORD(self.position_desired[ID])), DXL_HIBYTE(DXL_HIWORD(self.position_desired[ID]))]
+
+            dxl_addparam_result = self.pos_groupSyncWrite.addParam(ID, param_goal_position)
+
+            if dxl_addparam_result != True:
+
+                print("Failed to set Goal Position for Motor ID: " + str(ID) + ". groupSyncWrite addparam failed.\n")
+                self.shutdown()
+
+            else: 
+                print("Goal Position set to Motor ID: " + str(ID) + " to " + str(self.position_desired[ID] * 0.113) + "%." + "\n")
+
+
+        dxl_comm_result = self.pos_groupSyncWrite.txPacket()
+
+        if dxl_comm_result != COMM_SUCCESS:
+
+            print("Failed to group write Goal PWM.\n")
+            self.shutdown()
+
+        self.pos_groupSyncWrite.clearParam()
 
 
 
@@ -210,6 +270,36 @@ class Robot(object):
         self.pwm_groupSyncRead.clearParam()
 
 
+    def read_present_position(self):
+
+        for ID in self.DXL_ID:
+
+            dxl_addparam_result = self.pos_groupSyncRead.addParam(ID)
+
+            if dxl_addparam_result != True:
+                print("Failed setup for Motor ID: " + str(ID) + ". groupSyncRead addparam failed.\n")
+                self.shutdown()
+
+        dxl_comm_result = self.pos_groupSyncRead.txRxPacket()
+
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            print("Failed to group read Present Position.\n")
+            self.shutdown()
+
+        for ID in self.DXL_ID:
+
+            dxl_getdata_result = self.pos_groupSyncRead.isAvailable(ID, self.ADDR_PRESENT_POS, self.LEN_PRESENT_POS)
+
+            if dxl_getdata_result != True:
+                print("Failed to read Present Position from Motor ID: " + str(ID) + ". groupSyncRead getdata failed.\n")
+                self.shutdown()
+
+            self.DXL_pwm_present[ID] = self.pos_groupSyncRead.getData(ID, self.ADDR_PRESENT_POS, self.LEN_PRESENT_POS)
+
+        self.pos_groupSyncRead.clearParam()
+
+
 
     def shutdown(self):
 
@@ -234,9 +324,9 @@ def main():
 
     while not rospy.is_shutdown():
 
-        robot.set_goal_pwm()
-        robot.read_present_pwm()
-        robot.publish_present_pwm()
+        robot.set_goal_position()
+        robot.read_present_position()
+        robot.publish_present_position()
 
     rospy.on_shutdown(robot.shutdown())
 
---
+++ motor_control/src/motor_current_control.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+
+# --- Import libraries
+import os,rospy
+import numpy as np
+
+from std_msgs.msg import Int32MultiArray, Bool
+from dynamixel_sdk import *
+
+
+# --- Serial setup
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+
+# --- Define a Robot with multiple DYNAMIXEL motors. 
+class Robot(object):
+
+    # --- Joint Variables
+    current_desired = np.array([0,0,0,0,0]) # --- Desired Current
+    DXL_current_present = np.array([0,0,0,0,0]) # --- Present Current
+
+    # --- General Variables
+    DXL_ID = np.array([0,1,2,3,4])
+
+    # --- Addresses EEPROM
+    ADDR_OP_MODE = 11
+    ADDR_CURR_LIMIT = 38
+
+    # --- Addresses RAM
+    ADDR_TORQUE_EN = 64
+    ADDR_LED_EN = 65 
+    ADDR_GOAL_CURR = 102
+    ADDR_PRESENT_CURR = 126
+
+    # --- Byte Length
+    LEN_GOAL_CURR = 2
+    LEN_PRESENT_CURR = 2
+
+
+    # --- General Settings
+    PROT_VR = 2.0
+    BRATE = 1000000
+    DEVICE = '/dev/ttyUSB0'
+
+    portHandler = PortHandler(DEVICE)
+    packetHandler = PacketHandler(PROT_VR)
+
+    # --- Sync write and read instances
+    #pwm_groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_PWM, LEN_GOAL_PWM)
+    #pwm_groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_PWM, LEN_PRESENT_PWM)
+
+    curr_groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_CURR, LEN_GOAL_CURR)
+    curr_groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_CURR, LEN_PRESENT_CURR)
+
+
+
+    def __init__(self):
+
+        # --- Configure port
+        try:
+            self.portHandler.openPort()
+            print("\nSucceeded to open the port.\n")
+
+        except:
+            print("\nFailed to open the port.\n")
+            getch()
+            quit()
+
+        try:
+            self.portHandler.setBaudRate(self.BRATE)
+            print("Baudrate changed to " + str(self.BRATE) + "\n")
+        except:
+            print("Failed to change the baudrate.\n")
+            getch()
+            quit()
+
+
+        # --- ROS COMMS
+        self.currSubscriber = rospy.Subscriber("curr_goal_value",Int32MultiArray, self.update_goal_current)
+        rospy.sleep(0.005) #pwm_goal_value
+
+        self.currPublisher = rospy.Publisher("curr_present_value",Int32MultiArray, queue_size=10)
+        rospy.sleep(0.005) #pos_goal_value
+
+
+        # --- Set up Servomotors 
+        for ID in self.DXL_ID:
+
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_LED_EN, True)
+            
+            # --- Define Current Control / 0~1193 at 2.69mA per unit
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_OP_MODE, 0)
+            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, ID, self.ADDR_CURR_LIMIT, 125) 
+
+            dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_TORQUE_EN, True)
+
+            # --- Verify errors
+            if dxl_comm_result != COMM_SUCCESS:
+
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+                print("Failed setup for Motor ID: " + str(ID) + "\n")
+                self.shutdown()
+
+            elif dxl_error != 0:
+
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+                print("Failed setup for Motor ID: " + str(ID) + "\n")
+                self.shutdown()
+
+            else:
+                print("Motor ID: " + str(ID) + " ready to use!\n")
+
+
+
+    def update_goal_current(self,msg):
+
+        data_array = msg.data # --- Int32MultiArray data
+
+        if len(data_array) == len(self.DXL_ID):
+            self.current_desired = np.array(data_array)
+
+        else:
+            print("Invalid Current array, try again.\n")
+
+
+
+    def publish_present_current(self):
+
+        pub_array = Int32MultiArray()
+        pub_array.data = self.DXL_current_present.tolist()
+
+        self.currPublisher.publish(pub_array)
+    
+
+
+    def set_goal_current(self):
+
+        for ID in self.DXL_ID:
+
+            param_goal_curr = [DXL_LOBYTE(self.current_desired[ID]), DXL_HIBYTE(self.current_desired[ID])]
+
+            dxl_addparam_result = self.curr_groupSyncWrite.addParam(ID, param_goal_curr)
+
+            if dxl_addparam_result != True:
+
+                print("Failed to set Goal Current for Motor ID: " + str(ID) + ". groupSyncWrite addparam failed.\n")
+                self.shutdown()
+
+            else: 
+                print("Goal Current set to Motor ID: " + str(ID) + " to " + str(self.current_desired[ID] * 2.69) + "mA." + "\n")
+
+
+        dxl_comm_result = self.curr_groupSyncWrite.txPacket()
+
+        if dxl_comm_result != COMM_SUCCESS:
+
+            print("Failed to group write Goal Current.\n")
+            self.shutdown()
+
+        self.curr_groupSyncWrite.clearParam()
+
+
+
+    def read_present_current(self):
+
+        for ID in self.DXL_ID:
+
+            dxl_addparam_result = self.curr_groupSyncRead.addParam(ID)
+
+            if dxl_addparam_result != True:
+                print("Failed setup for Motor ID: " + str(ID) + ". groupSyncRead addparam failed.\n")
+                self.shutdown()
+
+        dxl_comm_result = self.curr_groupSyncRead.txRxPacket()
+
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            print("Failed to group read Present Current.\n")
+            self.shutdown()
+
+        for ID in self.DXL_ID:
+
+            dxl_getdata_result = self.curr_groupSyncRead.isAvailable(ID, self.ADDR_PRESENT_CURR, self.LEN_PRESENT_CURR)
+
+            if dxl_getdata_result != True:
+                print("Failed to read Present Current from Motor ID: " + str(ID) + ". groupSyncRead getdata failed.\n")
+                self.shutdown()
+
+            self.DXL_current_present[ID] = self.curr_groupSyncRead.getData(ID, self.ADDR_PRESENT_CURR, self.LEN_PRESENT_CURR)
+
+        self.curr_groupSyncRead.clearParam()
+
+
+
+    def shutdown(self):
+
+        for ID in self.DXL_ID:
+            self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_LED_EN, False)
+            self.packetHandler.write1ByteTxRx(self.portHandler, ID, self.ADDR_TORQUE_EN, False)
+            print("Shutting down Motor ID: " + str(ID) + "\n")
+        
+        self.portHandler.closePort()
+        getch()
+        quit()
+
+
+
+def main():
+
+    rospy.init_node("pwm_node")
+    freq = 10
+    rate = rospy.Rate(freq)
+
+    robot = Robot()
+
+    while not rospy.is_shutdown():
+
+        robot.set_goal_current()
+        robot.read_present_current()
+        robot.publish_present_current()
+
+    rospy.on_shutdown(robot.shutdown())
+
+
+if __name__ == '__main__':
+    main()
